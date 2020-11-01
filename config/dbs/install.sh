335afb559252d501e6d22a1ef514784f5368f336
Vasco Ramos <vascoalramos@outlook.pt>
2020-11-01 16:35:58 +0000

    Add comments to scripts

--- config/dbs/install.sh
+++ config/dbs/install.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+# install postgres
 sudo apt update && sudo apt install -y postgresql postgresql-contrib
 
+# create user 'misago'
 sudo -u postgres createuser misago
 
+# create database 'misago'
 sudo -u postgres createdb misago
 
+# update user password
 sudo -u postgres psql -c "ALTER USER misago WITH ENCRYPTED PASSWORD 'secret'"
 
+# update user role to superuser
 sudo -u postgres psql -c "ALTER USER misago WITH SUPERUSER"
 
+# allow user 'misago' to access database 'misago'
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE misago to misago"
 
+# insert custom postgresql config
 sudo cat config/postgresql.conf > /etc/postgresql/12/main/postgresql.conf
 sudo cat config/pg_hba.conf > /etc/postgresql/12/main/pg_hba.conf
 
+# restart postgresql
 sudo systemctl restart postgresql
--- config/server/run.sh
+++ config/server/run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+
+# install packages
 sudo apt update
 sudo apt install -y vim libffi-dev libssl-dev sqlite3 libjpeg-dev libopenjp2-7-dev locales cron postgresql-client gettext python3-pip
 
-cd misago
+# export .env variables
+cd misago && set -o allexport && source .env && set +o allexport
 
-set -o allexport && source .env && set +o allexport
-
+# install python3 requirements
 pip3 install --upgrade pip && pip3 install -r requirements.txt && pip3 install -r requirements-plugins.txt
 
+# run server
 python3 manage.py migrate && python3 manage.py runserver 0.0.0.0:8000
