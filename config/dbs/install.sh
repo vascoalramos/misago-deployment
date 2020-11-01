f8e68ac8b97d0ad714e660a026bec62076b04bef
Vasco Ramos <vascoalramos@outlook.pt>
2020-11-01 16:48:43 +0000

    Finish scripts

--- config/dbs/install.sh
+++ config/dbs/install.sh
@@ -19,8 +19,8 @@ sudo -u postgres psql -c "ALTER USER misago WITH SUPERUSER"
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE misago to misago"
 
 # insert custom postgresql config
-sudo cat config/postgresql.conf > /etc/postgresql/12/main/postgresql.conf
-sudo cat config/pg_hba.conf > /etc/postgresql/12/main/pg_hba.conf
+sudo bash -c 'cat config/postgresql.conf > /etc/postgresql/12/main/postgresql.conf'
+sudo bash -c 'cat config/pg_hba.conf > /etc/postgresql/12/main/pg_hba.conf'
 
 # restart postgresql
 sudo systemctl restart postgresql
